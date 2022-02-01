dec2b10e61a39835df65b7c49788128557003cfe
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-01 18:47:57 +0530

    use bodyParser

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
