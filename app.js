1ebf589b6a9dbbc73cd7a1b6e8dd2bcbd35b4a2f
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-01 18:57:11 +0530

    use app.post

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
