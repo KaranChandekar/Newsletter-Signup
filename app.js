eb4d19195d8b777d05bd17ed0b1bf5ff0eb88696
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-02 20:02:49 +0530

    change data type

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -14,11 +15,11 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  var data = {
+  const data = {
     members: [
       {
         email_address: email,
@@ -30,6 +31,9 @@ app.post("/", function (req, res) {
       },
     ],
   };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://<dc>.api.mailchimp.com/3.0/";
 });
 
 app.listen(3000, function () {
